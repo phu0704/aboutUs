b4ce20e63753622b5edb0770a3a7c470509feadc
phu0704 <dovanphu1405@gmail.com>
2023-08-07 05:21:15 +0700

    create branch phudv3

--- app.js
+++
@@ -1,8 +0,0 @@
-function showSideBar() {
-  let x = document.getElementById("sidebar");
-  if (x.className === "sidebar") {
-    x.className += " sidebar-show";
-  } else {
-    x.className = "sidebar";
-  }
-}
